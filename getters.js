420201414240336591612014cc44a929bd48227e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-08 09:45:30 -0500

    Add needed functions

---
+++ getters.js
@@ -0,0 +1,11 @@
+function getNumber() {
+    return document.getElementById('number').value;
+}
+
+function getInitalBase() {
+    return document.getElementById('initial-base').value;
+}
+
+function getFinalBase() {
+    return document.getElementById('final-base').value;
+}   
