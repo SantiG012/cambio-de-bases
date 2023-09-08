e8680af776956ad224dafbd1c088611b01135dd7
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-08 09:54:07 -0500

    Add the execution functions

--- validations.js
+++ validations.js
@@ -35,3 +35,60 @@ function areBasesEqual(initialBase, finalBase) {
     }
     return false;
 }
+
+function executeBasicValidations(number, initialBase, finalBase){
+
+    if (!isNumberInBase(number, initialBase)) {
+        return "Please enter a valid number in the initial base";
+    }
+
+    if (areBasesEqual(initialBase, finalBase)) {
+        return "The bases must be different";
+    }
+
+    return "";
+}
+
+function executeNullValidations(number,initialBase,finalBase){
+    if (isNull(number)) {
+        return "Please enter a number";
+    }
+
+    if (isNull(initialBase)) {
+        return "Please enter an initial base";
+    }
+
+    if (isNull(finalBase)) {
+        return "Please enter a final base";
+    }
+
+    return "";
+}
+
+function executeIntegerValidations(number,initialBase,finalBase){
+    if (!isInteger(number)) {
+        return "Please enter an integer number";
+    }
+
+    if (!isInteger(initialBase)) {
+        return "Please enter an integer initial base";
+    }
+
+    if (!isInteger(finalBase)) {
+        return "Please enter an integer final base";
+    }
+
+    return "";
+}
+
+function executeBaseRangeValidations(initialBase, finalBase){
+    if (!isBaseInRange(initialBase)) {
+        return "Please enter a valid initial base";
+    }
+
+    if (!isBaseInRange(finalBase)) {
+        return "Please enter a valid final base";
+    }
+
+    return "";
+}
