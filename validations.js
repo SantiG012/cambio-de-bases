8e2985f68d4dfca5aea5df326dc521e8156b3658
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-08 09:26:28 -0500

    Change functions names

--- validations.js
+++ validations.js
@@ -1,4 +1,4 @@
-function validateBaseInRange(base) {
+function isBaseInRange(base) {
     const base = parseInt(base);
 
     if (base < 2 || base > 13) {
@@ -8,20 +8,20 @@ function validateBaseInRange(base) {
     return true;
 }
 
-function validateNumberInBase(number, base) {
+function isNumberInBase(number, base) {
     const regex = REGEX_EXPRESSIONS[base];
     return regex.test(number);
 }
 
-function validateNotNull(number) {
+function isNull(number) {
     if (number === "" || number === null) {
-        return false;
+        return true;
     }
 
-    return true;
+    return false;
 }
 
-function validateInteger(number) {
+function isInteger(number) {
     if (number.includes(".")) {
         return false;
     }
@@ -29,10 +29,9 @@ function validateInteger(number) {
     return true;
 }
 
-function validateEqualBases(initialBase, finalBase) {
+function areBasesEqual(initialBase, finalBase) {
     if (initialBase === finalBase) {
         return true;
     }
-
     return false;
 }
